fc54a0bd8ad9fb3b8a10cbe14c5cec8997863faa
Raymart De Guzman <tramyardg@gmail.com>
2018-06-14 21:18:39 -0400

    moved react lessons to its own folder

--- fe-lib/react/jsx-elem.js
+++ fe-lib/react/jsx-elem.js
@@ -10,3 +10,11 @@ const JSX = (
     </div>
 );
 
+// # Add Comments in JSX
+const JSXcomments = (
+    <div>
+      {/* */}
+      <h1>This is a block of JSX</h1>
+      <p>Here's a subtitle</p>
+    </div>
+);
