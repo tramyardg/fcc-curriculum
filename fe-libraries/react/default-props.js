28ec3b0fdb4ffa4bcbb3718fc5333f3cce0bbb3e
Raymart De Guzman <tramyardg@gmail.com>
2018-06-22 22:36:04 -0400

    overriding default props

--- fe-libraries/react/default-props.js
+++ fe-libraries/react/default-props.js
@@ -11,3 +11,25 @@ const ShoppingCart = (props) => {
 ShoppingCart.defaultProps = {
   items: 0
 };
+
+// Override default props example
+const Items = (props) => {
+  return <h1>Current quantity: {props.qty}</h1>
+};
+
+Items.defaultsProps = {
+  qty: 0
+};
+
+class MyShoppingCart extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+        <div>
+          <Items qty={10}/>
+        </div>);
+  }
+}
